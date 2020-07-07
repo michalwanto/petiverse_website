ba91b0488d797e235f3141acbe2cbfeba21d39d2
Michal Wanto <michalwanto@192-168-1-132.tpgi.com.au>
2020-07-07 22:00:34 +1000

    seperate our cra app into client folder, added express server to help handle backend stripe

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,25 +1,41 @@
-import React from 'react';
-import StripeCheckout from 'react-stripe-checkout';
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
-  const publishableKey = 'pk_test_WBqax2FWVzS9QlpJScO07iuL';
+  const publishableKey =
+    "pk_test_51H1v3eDwIwtpSHLmeJHly8vs5meReOCmG0mAA9ZR96NQ4Qx4DNf833b0yg6VnuV3DtfObp2hssXTasQvRpWiPskl00gzAH8YHv";
 
-  const onToken = token => {
-    console.log(token);
-    alert('Payment Succesful!');
+  const onToken = (token) => {
+    debugger;
+    axios({
+      url: "payment",
+      method: "post",
+      data: {
+        amount: priceForStripe,
+        token,
+      },
+    })
+      .then((response) => {
+        alert("Payment Successful");
+      })
+      .catch((error) => {
+        console.log("Payment Error: ", JSON.parse(error));
+        alert(
+          "There was an error with your payment. Please ensure that you use the rigth payment"
+        );
+      });
   };
 
   return (
     <StripeCheckout
-      label='Pay Now'
-      name='CRWN Clothing Ltd.'
-      billingAddress
-      shippingAddress
-      image='https://svgshare.com/i/CUz.svg'
+      label="Pay Now"
+      name="CRWN Clothing Ltd."
+      image="https://svgshare.com/i/CUz.svg"
       description={`Your total is $${price}`}
       amount={priceForStripe}
-      panelLabel='Pay Now'
+      panelLabel="Pay Now"
       token={onToken}
       stripeKey={publishableKey}
     />
