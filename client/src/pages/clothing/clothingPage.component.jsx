0652f160a922dd5d5b1b54507373ed15d6f00419
Michal Wanto <michalwanto@192-168-1-132.tpgi.com.au>
2020-07-24 22:55:16 +1000

    added the clothing page

--- client/src/pages/clothing/clothingPage.component.jsx
+++ client/src/pages/clothing/clothingPage.component.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { gsap } from "gsap";
+import { Draggable } from "gsap/Draggable";
+
+gsap.registerPlugin(Draggable);
 
 const ClothingPage = () => {
-  return <div>ClothingPage</div>;
+  useEffect(() => {
+    Draggable.create(".picture");
+  });
+  return (
+    <div>
+      <h1>ClothingPage</h1>
+      <img
+        className="picture"
+        src={require("./png-pictures/dog-pict.png")}
+      ></img>
+      <img className="picture" src={require("./png-pictures/cloth1.png")}></img>
+      <img className="picture" src={require("./png-pictures/cloth2.png")}></img>
+    </div>
+  );
 };
 
 export default ClothingPage;
