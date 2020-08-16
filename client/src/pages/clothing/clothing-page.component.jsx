3870f4efd693f0f068f8cf4331e794ee78123ac6
Michal Wanto <michalwanto@MacBook-Pro-2.local>
2020-08-16 16:14:20 +1000

    remove images

--- client/src/pages/clothing/clothing-page.component.jsx
+++ client/src/pages/clothing/clothing-page.component.jsx
@@ -8,15 +8,10 @@ import "./clothing-page.styles.scss";
 gsap.registerPlugin(Draggable);
 
 const ClothingPage = () => {
-  useEffect(() => {
-    Draggable.create(".picture");
-  });
   return (
     <div>
       <h1>ClothingPage</h1>
 
-      <img className="picture" src={require("./png-pictures/cloth1.png")}></img>
-      <img className="picture" src={require("./png-pictures/cloth2.png")}></img>
       <FileLoader />
     </div>
   );
