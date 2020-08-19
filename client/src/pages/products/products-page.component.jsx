b3a5b5662f80f90565a1a1d82a06dbe69e051c6b
Michal Wanto <michalwanto@192-168-1-132.tpgi.com.au>
2020-08-19 20:51:06 +1000

    apply stripe

--- client/src/pages/products/products-page.component.jsx
+++ client/src/pages/products/products-page.component.jsx
@@ -2,15 +2,13 @@ import React, { useEffect } from "react";
 import { gsap } from "gsap";
 import { Draggable } from "gsap/Draggable";
 
-import { ProductsPageContainer, ProductImage } from "./products-page.styles";
-
 gsap.registerPlugin(Draggable);
 
 const ProductsPage = () => {
   useEffect(() => {
     Draggable.create(".picture");
   });
-  return <ProductsPageContainer></ProductsPageContainer>;
+  return;
 };
 
 export default ProductsPage;
