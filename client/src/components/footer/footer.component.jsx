2be27f45513eef68f3863b1f32a5c57bfe026e6f
Michal Wanto <michalwanto@MacBook-Pro-2.local>
2020-08-22 16:00:30 +1000

    finish product page

--- client/src/components/footer/footer.component.jsx
+++ client/src/components/footer/footer.component.jsx
@@ -1,8 +1,30 @@
 import React from "react";
 import "./footer.styles.css";
 
-const footer = () => {
-  return <div className="footerContainer"></div>;
+const Footer = () => {
+  return (
+    <div className="footerContainer">
+      <h3 className="send">SEND</h3>
+      <h1 className="enterThePetiverse">ENTER THE PETIVERSE</h1>
+      <input
+        className="footerInput"
+        type="textInput"
+        placeholder="Email"
+      ></input>
+      <h3 className="instagram">Instagram</h3>
+      <h3 className="facebook">Facebook</h3>
+
+      <h3 className="aboutUs">About Us</h3>
+      <h3 className="missionAndVision">Mission and Vision</h3>
+      <h3 className="community">Community</h3>
+      <h3 className="securityAndPrivacy">Security and Privacy</h3>
+      <h3 className="termsOfUse">Terms of Use</h3>
+
+      <h3 className="contactUs">Contact Us</h3>
+      <h3 className="shipping">Shipping</h3>
+      <h3 className="returns">Returns</h3>
+    </div>
+  );
 };
 
-export default footer;
+export default Footer;
