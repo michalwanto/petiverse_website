b94e2c17cf2794ecf01b38ff8266cb6e41b6092a
Michal Wanto <michalwanto@192-168-1-132.tpgi.com.au>
2020-08-26 16:23:49 +1000

    added margin on hamburger title

--- client/src/components/drawer/drawer.styles.jsx
+++ client/src/components/drawer/drawer.styles.jsx
@@ -5,7 +5,7 @@ import ListItem1 from "@material-ui/core/ListItem";
 export const HamburgerTitle = styled(Link)`
   font-family: TS Block;
   font-size: 15px;
-  justify-content: center;
+  margin-left: 10px;
 `;
 
 export const ListItem = styled(ListItem1)`
